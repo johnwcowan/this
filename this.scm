d79bde89ba818b4087ae2e75be4ab16717db053c
John Cowan <cowan@ccil.org>
2020-03-01 09:07:53 -0500

    Diluted the joke a bit, but made it more Schemey

--- this.scm
+++ this.scm
@@ -1,5 +1,23 @@
 (module this ()
 (import scheme)
-(display "this is bullshit.")
+(display
+"If you think this is bullshit, it probably is.  Nevertheless:
+Beautiful is better than ugly.
+Explicit renaming is faster, but not clearer, than implicit.
+Simple outsides hide complex insides.
+Nested is what Lisp is all about.
+Readability counts.
+It's all special cases, and that's why we have macros.
+Practicality and purity are equally important.
+Errors should never pass silently.
+Unless speed is set higher than safety.
+In the face of ambiguity, refuse the temptation to guess.
+There should be every way to do it.
+Now is better than never.
+But think before you code.
+If the implementation is hard to explain, explain it.
+If the implementation is easy to explain, explain it.
+There is (almost) only one namespace.
+")
 (newline)
 ) ;; end module
